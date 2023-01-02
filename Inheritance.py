cec96cdb91881a6f2c6f48c9efa056aef647a859
Mohamed Anwar <alhamed202024@gmail.com>
2023-01-02 16:54:04 +0300

    Inheritance 2

--- Inheritance.py
+++ Inheritance.py
@@ -36,3 +36,55 @@ class Apple(Food):  # Derived Class
 food_two = Apple("Pizza", 150, 500)
 food_two.eat()
 food_two.get_from_tree()
+
+
+
+# ---------------------------------------------------------
+# -- Object Oriented Programming => Multiple Inheritance --
+# ---------------------------------------------------------
+
+class BaseOne:
+
+  def __init__(self):
+
+    print("Base One")
+
+  def func_one(self):
+
+    print("One")
+
+class BaseTwo:
+
+  def __init__(self):
+
+    print("Base Two")
+
+  def func_two(self):
+
+    print("Two")
+
+class Derived(BaseOne, BaseTwo):
+
+  pass
+
+my_var = Derived()
+
+# print(Derived.mro())
+
+print(my_var.func_one)
+print(my_var.func_two)
+
+my_var.func_one()
+my_var.func_two()
+
+class Base:
+
+  pass
+
+class DerivedOne(Base):
+
+  pass
+
+class DerivedTwo(DerivedOne):
+
+  pass
