43de829853f580023239be2f4413441c9997f241
Mohamed Anwar <alhamed202024@gmail.com>
2023-01-03 17:01:38 +0300

    Regular Expressions 2

--- Regular_Expressions.py
+++ Regular_Expressions.py
@@ -8,3 +8,14 @@
 # [5] Characters Sheet "https://www.debuggex.com/cheatsheet/regex/python"
 # -----------------------------------------------------------
 
+# ----------------------------------------
+# -- Regular Expressions => Quantifiers --
+# ----------------------------------------
+# *	0 or more
+# +	1 or more
+# ?	0 or 1
+# {2}	Exactly 2
+# {2, 5}	Between 2 and 5
+# {2,}	2 or more
+# (,5}	Up to 5
+# -------------
